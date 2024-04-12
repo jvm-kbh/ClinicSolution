9e647626cffe0410fd85a502da8dbafadefe09ab
jvm-kbh <elasticsearch@kakao.com>
2024-04-12 23:43:47 +0900

    feat: set up git ignore

---
+++ src/main/java/me/kbh/clinicsolution/ClinicSolutionApplication.java
@@ -0,0 +1,13 @@
+package me.kbh.clinicsolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicSolutionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ClinicSolutionApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/kbh/clinicsolution/ClinicSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kbh.clinicsolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicSolutionApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
