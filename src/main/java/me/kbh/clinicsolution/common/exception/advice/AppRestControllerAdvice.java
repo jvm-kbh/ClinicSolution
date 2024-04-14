a07bdcb16851030e78c6ff84d11a2bde2f2b0584
jvm-kbh <elasticsearch@kakao.com>
2024-04-14 16:27:32 +0900

    feat: Register for a business error - SystemGroupCode, SystemCode

--- src/main/java/me/kbh/clinicsolution/common/exception/advice/AppRestControllerAdvice.java
+++ src/main/java/me/kbh/clinicsolution/common/exception/advice/AppRestControllerAdvice.java
@@ -1,9 +1,25 @@
 package me.kbh.clinicsolution.common.exception.advice;
 
+import me.kbh.clinicsolution.common.exception.response.system.SystemCodeBusinessException;
+import me.kbh.clinicsolution.common.exception.response.system.SystemCodeErrorResponse;
+import me.kbh.clinicsolution.common.exception.response.system.SystemGroupCodeBusinessException;
+import me.kbh.clinicsolution.common.exception.response.system.SystemGroupCodeErrorResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class AppRestControllerAdvice {
 
+  @ExceptionHandler(SystemCodeBusinessException.class)
+  public ResponseEntity<SystemCodeErrorResponse> handlingForSystemCodeBusinessException(
+      SystemCodeBusinessException exception) {
+    return SystemCodeErrorResponse.toResponse(exception.getSystemCodeError());
+  }
 
+  @ExceptionHandler(SystemGroupCodeBusinessException.class)
+  public ResponseEntity<SystemGroupCodeErrorResponse> handlingForSystemGroupCodeBusinessException(
+      SystemGroupCodeBusinessException exception) {
+    return SystemGroupCodeErrorResponse.toResponse(exception.getSystemGroupCodeError());
+  }
 }
