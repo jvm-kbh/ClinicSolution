484702b7df8d0f6547131bf2132a9b0b7825592c
jvm-kbh <elasticsearch@kakao.com>
2024-04-15 14:20:48 +0900

    fix: update request parameter size

--- src/main/java/me/kbh/clinicsolution/domain/hospital/dto/HospitalUpdateRequest.java
+++ src/main/java/me/kbh/clinicsolution/domain/hospital/dto/HospitalUpdateRequest.java
@@ -1,6 +1,7 @@
 package me.kbh.clinicsolution.domain.hospital.dto;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,10 +13,15 @@ import lombok.experimental.FieldDefaults;
 public class HospitalUpdateRequest {
 
   @NotNull(message = "병원 이름은 비어있을 수 없습니다.")
+  @Size(max = 45,message = "병원 이름의 최대 길이는 45입니다.")
   String hospitalName;
+
   @NotNull(message = "기관 번호는 비어있을 수 없습니다.")
+  @Size(max = 20,message = "기관 번호의 최대 길이는 20입니다.")
   String medicalInstitutionNumber;
+
   @NotNull(message = "병원장 이름은 비어있을 수 없습니다.")
+  @Size(max = 10,message = "병원장 이름의 최대 길이는 10입니다.")
   String hospitalDirectorName;
 
 }
